20b82787a8d20b86877f745ce79ab0197fca01ad
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-26 23:16:40 +0545

    Modified

--- src/services/GetServices.jsx
+++ src/services/GetServices.jsx
@@ -1,11 +1,9 @@
-// src/services/GetServices.jsx
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+import React from 'react';
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 const fetchWeatherData = async (location) => {
   try {
-    const response = await fetch(`${API_BASE_URL}?key=${API_KEY}&q=${location}&aqi=no`);
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=no`);
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
